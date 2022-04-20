ac492319f11257fd57552be72671a3db03f59071
Aozaki <aozaki.kuro@gmail.com>
2022-04-20 18:12:01 +0800

    delete unused components

--- layouts/Commission.tsx
+++ layouts/Commission.tsx
@@ -1,4 +1,3 @@
-import Link from '@/components/Link'
 import PageTitle from '@/components/PageTitle'
 import SectionContainer from '@/components/SectionContainer'
 import { BlogSEO } from '@/components/SEO'
@@ -12,11 +11,9 @@ import type { Blog } from 'contentlayer/generated'
 interface Props {
   content: CoreContent<Blog>
   children: ReactNode
-  next?: { slug: string; title: string }
-  prev?: { slug: string; title: string }
 }
 
-export default function PostLayout({ content, next, prev, children }: Props) {
+export default function PostLayout({ content, children }: Props) {
   const { slug, date, title } = content
 
   return (
@@ -47,30 +44,6 @@ export default function PostLayout({ content, next, prev, children }: Props) {
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
-            <footer>
-              <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
-                {prev && (
-                  <div className="pt-4 xl:pt-8">
-                    <Link
-                      href={`/blog/${prev.slug}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      &larr; {prev.title}
-                    </Link>
-                  </div>
-                )}
-                {next && (
-                  <div className="pt-4 xl:pt-8">
-                    <Link
-                      href={`/blog/${next.slug}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      {next.title} &rarr;
-                    </Link>
-                  </div>
-                )}
-              </div>
-            </footer>
           </div>
         </div>
       </article>
