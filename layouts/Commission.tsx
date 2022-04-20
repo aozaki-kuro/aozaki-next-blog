56fa0be3d2b1bc33c75430481500d99a8dd2744f
Aozaki <aozaki.kuro@gmail.com>
2022-04-20 18:21:13 +0800

    fix layout

--- layouts/Commission.tsx
+++ layouts/Commission.tsx
@@ -42,7 +42,7 @@ export default function PostLayout({ content, children }: Props) {
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+              <div className="prose max-w-none pt-10 pb-0 dark:prose-dark">{children}</div>
             </div>
           </div>
         </div>
