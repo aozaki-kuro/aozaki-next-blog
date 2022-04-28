f9cd8fb3453747982f5e6abffa84b3a1e705407b
Aozaki <aozaki.kuro@gmail.com>
2022-04-28 20:41:33 +0800

    adjust project listing

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,64 +1,5 @@
 const projectsData = [
-  {
-    title: 'Portrait: Memorial',
-    description: `© 2014 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20140327_0005.jpg',
-    href: '/blog/portrait/memorial',
-  },
-  {
-    title: 'Portrait: Blossom',
-    description: `© 2015 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20150209_0008.jpg',
-    href: '/blog/portrait/blossom',
-  },
-  {
-    title: 'Portrait: Regret',
-    description: `© 2015 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20150329_0009.jpg',
-    href: '/blog/portrait/regret',
-  },
-  {
-    title: 'Portrait: Refusal',
-    description: `© 2015 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20150726_0008.jpg',
-    href: '/blog/portrait/refusal',
-  },
-  {
-    title: 'Portrait: Funeral',
-    description: `© 2015 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20150809_0005.jpg',
-    href: '/blog/portrait/funeral',
-  },
-  {
-    title: 'Portrait: Awareness',
-    description: `© 2015 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20151125_0002.jpg',
-    href: '/blog/portrait/awareness',
-  },
-  {
-    title: 'Portrait: Origami',
-    description: `© 2016 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20160505_0002.jpg',
-    href: '/blog/portrait/origami',
-  },
-  {
-    title: 'Portrait: Unbalance',
-    description: `© 2016 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20160715_0001.jpg',
-    href: '/blog/portrait/unbalance',
-  },
-  {
-    title: 'Portrait: Wander',
-    description: `© 2017 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20170720_0008.jpg',
-    href: '/blog/portrait/wander',
-  },
-  {
-    title: 'Portrait: Dreamer',
-    description: `© 2019 Aozaki`,
-    imgSrc: 'https://img.aozaki.cc/20190214_0001.jpg',
-    href: '/blog/portrait/dreamer',
-  },
+  /** Recent Works */
   {
     title: 'Art Commission',
     description: `The record of art commission. DO NOT REPOST.`,
@@ -70,6 +11,68 @@ const projectsData = [
     description: `A Wiki site to record Tokomachi activities and details. This wiki is fan-made, unofficial and non-profit.`,
     imgSrc: '/static/images/tokomachi_wiki.jpg',
     href: 'https://toko.suisei.cc',
+  },
+
+  /** Photography */
+  {
+    title: 'Portrait: Dreamer',
+    description: `© 2019 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20190214_0001.jpg',
+    href: '/blog/portrait/dreamer',
+  },
+  {
+    title: 'Portrait: Wander',
+    description: `© 2017 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20170720_0008.jpg',
+    href: '/blog/portrait/wander',
+  },
+  {
+    title: 'Portrait: Unbalance',
+    description: `© 2016 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20160715_0001.jpg',
+    href: '/blog/portrait/unbalance',
+  },
+  {
+    title: 'Portrait: Origami',
+    description: `© 2016 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20160505_0002.jpg',
+    href: '/blog/portrait/origami',
+  },
+  {
+    title: 'Portrait: Awareness',
+    description: `© 2015 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20151125_0002.jpg',
+    href: '/blog/portrait/awareness',
+  },
+  {
+    title: 'Portrait: Funeral',
+    description: `© 2015 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20150809_0005.jpg',
+    href: '/blog/portrait/funeral',
+  },
+  {
+    title: 'Portrait: Refusal',
+    description: `© 2015 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20150726_0008.jpg',
+    href: '/blog/portrait/refusal',
+  },
+  {
+    title: 'Portrait: Regret',
+    description: `© 2015 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20150329_0009.jpg',
+    href: '/blog/portrait/regret',
+  },
+  {
+    title: 'Portrait: Blossom',
+    description: `© 2015 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20150209_0008.jpg',
+    href: '/blog/portrait/blossom',
+  },
+  {
+    title: 'Portrait: Memorial',
+    description: `© 2014 Aozaki`,
+    imgSrc: 'https://img.aozaki.cc/20140327_0005.jpg',
+    href: '/blog/portrait/memorial',
   },
 ]
 
