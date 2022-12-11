4afef05a80efb2621fb9be9382c9ee91724f36d4
Aozaki Kuro <i@aozaki.cc>
2022-12-11 15:09:34 +0800

    add info for skeb commissions

--- components/Caption.tsx
+++ components/Caption.tsx
@@ -3,19 +3,22 @@ import Link from 'next/link'
 const Caption = ({
   Year,
   Name,
+  Skeb,
   Url
 }: {
   Year: string
   Name: string
   Url: string
+  Skeb: boolean
 }) => {
+  let displaySkeb = Skeb == true ? `/ Skeb` : null
   return (
     <div className="centered">
       © {Year} [{' '}
       <Link href={Url} target="_blank" rel="noreferrer">
         {Name}
       </Link>{' '}
-      ]
+      {displaySkeb} ]
     </div>
   )
 }
