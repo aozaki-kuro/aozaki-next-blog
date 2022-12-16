bde6ba1cb455641bc5d1d67cba0d70c0f9fe3675
Aozaki Kuro <i@aozaki.cc>
2022-12-16 20:41:14 +0800

    components for comission

---
+++ components/commission/Copyright.tsx
@@ -0,0 +1,12 @@
+const Copyright = () => {
+  return (
+    <span className="centered text-small">
+      規約による、Skeb
+      でやり取りされた作品のすべての権利はクリエイターと版権元に帰属する。 The
+      copyright of all art works commissioned on Skeb belongs to the artist and
+      the proper copyright holders. © Copyright COVER corp. & ANYCOLOR Inc.
+    </span>
+  )
+}
+
+export default Copyright
