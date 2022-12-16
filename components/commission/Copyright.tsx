f1cf883dadb8fa4e1ed9fa65e8bb53760d084a40
Aozaki Kuro <i@aozaki.cc>
2022-12-16 20:51:51 +0800

    fix copyright layout

--- components/commission/Copyright.tsx
+++ components/commission/Copyright.tsx
@@ -1,11 +1,15 @@
 const Copyright = () => {
   return (
-    <span className="centered text-small">
-      規約による、Skeb
-      でやり取りされた作品のすべての権利はクリエイターと版権元に帰属する。 The
-      copyright of all art works commissioned on Skeb belongs to the artist and
-      the proper copyright holders. © Copyright COVER corp. & ANYCOLOR Inc.
-    </span>
+    <div className="centered text-small">
+      <p>
+        規約による、Skebでやり取りされた作品のすべての権利はクリエイターと版権元に帰属する。
+      </p>
+      <p>
+        The copyright of all art works commissioned on Skeb belongs to the
+        artist and the proper copyright holders.
+      </p>
+      <p>© Copyright COVER corp. & ANYCOLOR Inc.</p>
+    </div>
   )
 }
 
