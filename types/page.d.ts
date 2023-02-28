aecceec8a6bf40d07707c9e158fc73adda0137c4
Aozaki Kuro <i@aozaki.cc>
2023-02-28 21:37:44 +0800

    del unsed files

--- types/page.d.ts
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next'
-import { ComponentType, ReactElement, ReactNode } from 'react'
-
-export type Page = NextPage & {
-  // You can disable whichever you don't need
-  getLayout?: (page: ReactElement) => ReactNode
-  layout?: ComponentType
-}
