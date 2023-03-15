4854a77a9ee94af2d196a5fdbe42b1e0c1ce155c
Aozaki Kuro <i@aozaki.cc>
2023-03-15 10:56:11 +0800

    use plausible analytics

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -2,11 +2,7 @@ import Script from 'next/script'
 
 const Analytics = () => {
   return (
-    <Script
-      strategy="afterInteractive"
-      data-website-id="0a65d059-5317-4bb9-bf8f-ad710d5dc95e"
-      src="https://sight.aozaki.cc/app.js"
-    />
+    <Script strategy="afterInteractive" src="https://sight.aozaki.cc/app.js" />
   )
 }
 
