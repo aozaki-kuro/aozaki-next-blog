1f4c9015e943ec89f2a435de254ff1c41c663934
Aozaki Kuro <i@aozaki.cc>
2023-05-18 17:59:01 +0800

    fix: listing and info

--- components/commission/illustrator-info.tsx
+++ components/commission/illustrator-info.tsx
@@ -1,34 +1,39 @@
-import { useMemo } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { Transition } from '@headlessui/react'
-import type { CommissionInfoProps } from 'CommissionInfoProps'
+import { CommissionInfoProps } from 'CommissionInfoProps'
 
 const IllustratorInfo = ({
-  PublishDate = '',
+  PublishDate,
   Creator,
   Twitter,
   Skeb,
   Pixiv
 }: CommissionInfoProps) => {
   // format the date
-  const formattedDate = useMemo(() => {
-    if (PublishDate.length < 8) return '\u2000'.repeat(10)
+  const [formattedDate, setFormattedDate] = useState<string>(
+    // For "20 /  /  "
+    '\u2000'.repeat(4) + '/' + '\u2000'.repeat(2) + '/' + '\u2000'.repeat(2)
+  )
+
+  useEffect(() => {
     const date = new Date(
       `${PublishDate.slice(0, 4)}-${PublishDate.slice(
         4,
         6
       )}-${PublishDate.slice(6, 8)}`
     )
-    return date.toLocaleDateString(undefined, {
+    const formattedDate = date.toLocaleDateString(undefined, {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit'
     })
+    setFormattedDate(formattedDate)
   }, [PublishDate])
 
   // create reusable function to generate links with proper styling
-  const createLink = (url?: string, text?: string) =>
-    url && text ? (
+  const createLink = (url: string, text: string) => {
+    return url ? (
       <>
         <Link href={url} className="underline-offset-[0.2rem]" target="_blank">
           {text}
@@ -36,6 +41,7 @@ const IllustratorInfo = ({
         <span className="pr-3 ss:pr-2" />
       </>
     ) : null
+  }
 
   // display illustrator information in a grid layout
   return (
@@ -47,9 +53,9 @@ const IllustratorInfo = ({
       enterTo="opacity-100"
     >
       <div className="flex flex-auto font-mono text-sm ss:text-xs">
-        <div>{formattedDate}</div>
+        <div className="">{formattedDate}</div>
         <span className="pr-16 ss:pr-6" />
-        <div>{Creator ?? '-'}</div>
+        <div className="">{Creator || '-'}</div>
         <div className="grow text-right">
           {'['}
           <span className="pr-3 ss:pr-2" />
