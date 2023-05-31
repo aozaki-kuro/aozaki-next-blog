205edc9fad238f01d18d76012c1054b393ac04e5
Aozaki Kuro <i@aozaki.cc>
2023-05-31 13:29:44 +0800

    chore(deps): update deps

--- scripts/gen-rss.mjs
+++ scripts/gen-rss.mjs
@@ -3,7 +3,6 @@ import { join } from 'path'
 import RSS from 'rss'
 import matter from 'gray-matter'
 
-// @TODO: for now let's generate with all posts, unsorted
 async function generate() {
   const feed = new RSS({
     title: "Aozaki's Blog",
@@ -11,23 +10,16 @@ async function generate() {
     feed_url: 'https://blog.aozaki.cc/feed.xml'
   })
 
-  const dirPath = join(
-    new URL('.', import.meta.url).pathname,
-    '..',
-    'pages',
-    'posts'
-  )
+  const dirPath = join('./pages/posts')
   const posts = await fs.readdir(dirPath)
 
-  for (let i = 0; i < posts.length; i++) {
-    if (posts[i].startsWith('index.')) continue
-
-    const content = await fs.readFile(join(dirPath, posts[i]))
+  for (const post of posts.filter(fileName => !fileName.startsWith('index.'))) {
+    const content = await fs.readFile(join(dirPath, post))
     const frontmatter = matter(content)
 
     feed.item({
       title: frontmatter.data.title,
-      url: '/posts/' + posts[i].replace(/\.mdx?/, ''),
+      url: `/posts/${post.replace(/\.mdx?/, '')}`,
       date: frontmatter.data.date,
       description: frontmatter.data.description,
       author: frontmatter.data.author
