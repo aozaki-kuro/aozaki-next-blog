d14d4890dd09fa9246966cd8a4b65e686ff8bb4b
Aozaki Kuro <i@aozaki.cc>
2023-06-07 19:30:12 +0800

    update: use ts-node and update tsconfig

--- scripts/gen-rss.ts
+++ scripts/gen-rss.ts
@@ -4,29 +4,46 @@ import RSS from 'rss'
 import matter from 'gray-matter'
 
 async function generate() {
-  const feed = new RSS({
-    title: "Aozaki's Blog",
-    site_url: 'https://blog.aozaki.cc',
-    feed_url: 'https://blog.aozaki.cc/feed.xml'
-  })
-
-  const dirPath = join('./pages/posts')
-  const posts = await fs.readdir(dirPath)
-
-  for (const post of posts.filter(fileName => !fileName.startsWith('index.'))) {
-    const content = await fs.readFile(join(dirPath, post))
-    const frontmatter = matter(content)
-
-    feed.item({
-      title: frontmatter.data.title,
-      url: `/posts/${post.replace(/\.mdx?/, '')}`,
-      date: frontmatter.data.date,
-      description: frontmatter.data.description,
-      author: frontmatter.data.author
+  try {
+    const feed = new RSS({
+      title: "Aozaki's Blog",
+      site_url: 'https://blog.aozaki.cc',
+      feed_url: 'https://blog.aozaki.cc/feed.xml'
     })
-  }
 
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+    const dirPath = join('./pages/posts')
+    const posts = await fs.readdir(dirPath)
+
+    for (const post of posts.filter(
+      fileName => !fileName.startsWith('index.')
+    )) {
+      const content = await fs.readFile(join(dirPath, post))
+      const frontmatter = matter(content)
+
+      feed.item({
+        title: frontmatter.data.title,
+        url: `/posts/${post.replace(/\.mdx?/, '')}`,
+        date: frontmatter.data.date,
+        description: frontmatter.data.description,
+        author: frontmatter.data.author
+      })
+    }
+
+    await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+
+    console.log(
+      '\x1b[42m%s\x1b[0m',
+      ' DONE ',
+      'RSS feed generated successfully!'
+    )
+  } catch (error) {
+    console.error(
+      '\x1b[41m%s\x1b[0m',
+      ' FAIL ',
+      'Failed to generate RSS feed:',
+      error
+    )
+  }
 }
 
 generate()
