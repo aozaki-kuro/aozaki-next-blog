54a71ab592c2c9724c005b272b43b31958ecf2d0
Aozaki Kuro <i@aozaki.cc>
2023-06-16 16:06:23 +0800

    fix: move components to avoid error

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,71 +1,16 @@
+import CustomHead from '#components/custom-head'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-
 import { FaGithub, FaMastodon, FaRss, FaTelegramPlane, FaTwitter } from 'react-icons/fa'
 
 // Fixed name and paths
-const Site = `Aozaki's blog`
-const twitterCard = `https://img.aozaki.cc/twitter-card.jpg`
 
 // Year
 const YEAR = new Date().getFullYear()
 
 // Nextra blog theme config
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+const BlogThemeConfig = {
   // <Head>
-  head: ({ meta }: { meta: { title: string; description: string; image: string } }) => {
-    // Get current route for Url
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const router = useRouter()  
-    const canonicalUrl = (
-      `https://blog.aozaki.cc` + (router.asPath === '/' ? '' : router.asPath)
-    ).split('?')[0]
-
-    // Get Current Title
-    const currentTitle = meta.title === `About` ? Site : `${meta.title} - ${Site}`
-
-    // Here goes the SEO part
-    return (
-      <>
-        {/* SEO : Traditional */}
-        <meta name="robots" content="noodp" />
-        <title>{currentTitle}</title>
-        <meta name="title" content={currentTitle} />
-        <meta name="author" content="Aozaki" />
-        <meta name="description" content={meta.description} />
-        <link rel="canonical" href={canonicalUrl} />
-
-        {/* SEO : Opengraph */}
-        <meta property="og:title" content={currentTitle} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={canonicalUrl} />
-        <meta property="og:image" content={meta.image || twitterCard} />
-        <meta name="og:site_name" content={Site} />
-
-        {/* SEO : Twitter Card */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image" content={meta.image || twitterCard} />
-        <meta property="twitter:title" content={currentTitle} />
-        <meta property="twitter:description" content={meta.description} />
-        <meta property="twitter:url" content={canonicalUrl} />
-        <meta property="twitter:site" content="@Aozaki__" />
-
-        {/* SEO : PWA realted */}
-        <meta name="application-name" content={Site} />
-        <meta name="apple-mobile-web-app-title" content={Site} />
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#6fa8dc" />
-
-        {/* SEO : RSS */}
-        <link rel="feed" href="/index.xml" type="application/rss+xml" title={Site} />
-      </>
-    )
-  },
+  head: CustomHead,
 
   // <Footer/>
   footer: (
@@ -125,3 +70,5 @@ export default {
     </div>
   )
 }
+
+export default BlogThemeConfig
