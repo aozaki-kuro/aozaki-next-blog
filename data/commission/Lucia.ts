c10aea42ed0b8cbb22bea3d40aa4ee73b50f59f9
Aozaki Kuro <i@aozaki.cc>
2023-07-03 07:39:42 +0800

    update: new commission

--- data/commission/Lucia.ts
+++ data/commission/Lucia.ts
@@ -8,5 +8,13 @@ export const Lucia: Props[] = [
     Twitter: 'https://twitter.com/Qooo003/status/1675005730800435201',
     Pixiv: '',
     Skeb: 'https://skeb.jp/@Qooo003/works/40'
-  }
+  },
+  {
+    fileName: '20230703_Lowrise',
+    Character: 'lucia',
+    Featured: false,
+    Twitter: '',
+    Pixiv: '',
+    Skeb: 'https://skeb.jp/@L0WR15E/works/11'
+  },
 ]
