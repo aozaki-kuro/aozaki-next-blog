f712968cc9667db0cef2120ef843b4dd192e64d2
Aozaki Kuro <i@aozaki.cc>
2023-12-13 18:08:19 +0800

    try: fix dark mode problem

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     'theme.config.tsx',
   ],
-  darkMode: 'class',
+  // darkMode: 'class',
   theme: {
     screens: {
       ss: { max: '480px' },
