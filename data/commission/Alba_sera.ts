1044ce5fd538169be69bba0ffa716de54795c873
Aozaki Kuro <i@aozaki.cc>
2024-03-05 19:16:42 +0800

    new: commission entry

--- data/commission/Alba_sera.ts
+++ data/commission/Alba_sera.ts
@@ -9,4 +9,12 @@ export const Sera: Props[] = [
     Pixiv: '',
     Skeb: 'https://skeb.jp/@hamident83hami/works/59',
   },
+  {
+    fileName: '20240305_Lowrise',
+    Character: 'sera',
+    Featured: false,
+    Twitter: 'https://twitter.com/L0WR15E/status/1764969104916848666',
+    Pixiv: '',
+    Skeb: 'https://skeb.jp/@L0WR15E/works/19',
+  },
 ]
