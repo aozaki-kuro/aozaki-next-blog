666e91c2cc4122f503db3f2e12394959c9b712d0
Aozaki Kuro <i@aozaki.cc>
2024-07-21 11:42:18 +0800

    fix: use plausible

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -8,8 +8,8 @@ const Analytics = () => {
   return (
     <Script
       strategy="afterInteractive"
-      src="https://sight.aozaki.cc/script.js"
-      data-website-id="a9c6dcd0-2c12-4342-a0cc-d1d9ba27b3d6"
+      src="https://sight.aozaki.cc/app.js"
+      data-domain="blog.aozaki.cc"
     />
   )
 }
