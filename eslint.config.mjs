da4018df104f65a4be09679c116253fd5705b93f
Aozaki Kuro <i@aozaki.cc>
2024-08-11 13:05:34 +0800

    try: migrate to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+
+import { FlatCompat } from '@eslint/eslintrc'
+import { fixupConfigRules } from '@eslint/compat'
+
+import ts from 'typescript-eslint'
+import prettierConfigRecommended from 'eslint-plugin-prettier/recommended'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+const patchedConfig = fixupConfigRules([...compat.extends('next/core-web-vitals')])
+
+const config = [
+  ...patchedConfig,
+  ...ts.configs.recommended,
+  prettierConfigRecommended,
+  {
+    ignores: ['**/.next/**', '**/_next/**', '**/dist/**', '**/out/**'],
+    rules: {
+      '@next/next/no-img-element': 'off',
+      'prefer-const': 'error',
+      '@typescript-eslint/ban-ts-comment': 0,
+      '@typescript-eslint/no-var-requires': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+    },
+  },
+]
+
+export default config
