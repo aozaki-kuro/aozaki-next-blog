0a7d1d486cc2d00e05492a2e83bdaa3aa9e52c98
Aozaki <i@aozaki.cc>
2024-12-29 11:08:48 +0800

    fix: eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,37 +1,19 @@
-import js from '@eslint/js'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-import { fixupConfigRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 import prettierConfigRecommended from 'eslint-plugin-prettier/recommended'
-import ts from 'typescript-eslint'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = dirname(__filename)
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 })
 
-const patchedConfig = fixupConfigRules([...compat.extends('next/core-web-vitals')])
-
-const config = [
-  ...patchedConfig,
-  ...ts.configs.recommended,
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   prettierConfigRecommended,
-  {
-    ignores: ['**/.next/**', '**/_next/**', '**/dist/**', '**/out/**'],
-    rules: {
-      '@next/next/no-img-element': 'off',
-      'prefer-const': 'error',
-      '@typescript-eslint/ban-ts-comment': 0,
-      '@typescript-eslint/no-var-requires': 0,
-      '@typescript-eslint/no-explicit-any': 0,
-    },
-  },
 ]
 
-export default config
+export default eslintConfig
