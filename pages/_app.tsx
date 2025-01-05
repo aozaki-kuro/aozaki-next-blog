b70389fa5948a135bbf022452632b5133c1a87b8
Aozaki <i@aozaki.cc>
2025-01-05 11:04:54 +0800

    new: bring menlo with site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,10 +24,23 @@ export const inter = localFont({
   ],
 })
 
+const menlo = localFont({
+  variable: '--font-menlo',
+  display: 'block',
+  style: 'normal',
+  src: [
+    {
+      path: '../public/fonts/Menlo-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+})
+
 // 定义 App 组件
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${inter.variable} font-sans`}>
+    <main className={`${inter.variable} ${menlo.variable} font-sans`}>
       {/* RSS 链接 */}
       <link rel="alternate" type="application/rss+xml" title="RSS" href="/feed.xml" />
 
